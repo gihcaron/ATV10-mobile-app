43fc7108fc261d846023886734b83c741ce4d56b
Giovanna <giovannacaron.tds1.senai@gmail.com>
2025-03-24 17:22:22 -0300

    Feat: create detalhes page

--- pages/Detalhes/Detalhes.js
+++ pages/Detalhes/Detalhes.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-const Detalhes = () => {
+const Detalhes = ({ route, navigation }) => {
+    const { inputText = 'Nenhum texto salvo', savedText = 'Nenhum texto salvo' } = route.params;
+
     return (
-        <View>
-            <Text>Detalhes Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Detalhes</Text>
+            <Text style={styles.red_text}>Sem Persistência: {inputText}</Text>
+            <Text style={styles.green_text}>Persistência: {savedText}</Text>
+            <View style={styles.buttonContainer}>
+                <Button title='Voltar' onPress={() => navigation.goBack()} color="blue" />
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+
+    title: {
+        fontSize: 20,
+        fontWeight: '600',
+        textAlign: 'center',
+        margin: 10,
+    },
+
+    red_text: {
+        color: 'red',
+        marginBottom: 10,
+    },
+
+    green_text: {
+        color: 'green',
+        marginBottom: 10,
+    },
+    
+});
 
 export default Detalhes;
